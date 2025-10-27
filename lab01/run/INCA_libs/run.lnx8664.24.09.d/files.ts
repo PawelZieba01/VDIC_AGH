cb6cd0bae9863f8d9e1afcacd74fadc434778608
PawelZieba01 <zpawel.zieba@gmail.com>
2025-10-27 13:39:56 +0100

    update lab1

--- lab01/run/INCA_libs/run.lnx8664.24.09.d/files.ts
+++ lab01/run/INCA_libs/run.lnx8664.24.09.d/files.ts
@@ -1,2 +1,2 @@
 1760080842 /home/student/pzieba/VDIC/common/simple_uart_switch.svp
-1761014913 /home/student/pzieba/VDIC/lab01/tb/simple_uart_switch_tb.sv
+1761568058 /home/student/pzieba/VDIC/lab01/tb/simple_uart_switch_tb.sv
