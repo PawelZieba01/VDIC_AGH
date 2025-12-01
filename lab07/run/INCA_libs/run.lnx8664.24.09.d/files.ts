8de3ba745c08103939e3a7c51ae7d157ec175fb1
PawelZieba01 <zpawel.zieba@gmail.com>
2025-12-01 20:53:49 +0100

    Add working lab07

---
+++ lab07/run/INCA_libs/run.lnx8664.24.09.d/files.ts
@@ -0,0 +1,4 @@
+1760080842 /home/student/pzieba/VDIC/common/simple_uart_switch.svp
+1764601022 /home/student/pzieba/VDIC/lab07/tb/fifomult_tb_pkg.sv
+1764618346 /home/student/pzieba/VDIC/lab07/tb/switch_bfm.sv
+1764554754 /home/student/pzieba/VDIC/lab07/tb/top.sv
